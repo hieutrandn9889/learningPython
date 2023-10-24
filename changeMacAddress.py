af9f879cc95bcdd770b08ebe5fb1a6733ac38090
Hieu Tran <hieu.tran@enouvo.com>
2023-10-24 09:11:27 +0700

    update regex

--- changeMacAddress.py
+++ changeMacAddress.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import re
 # ifconfig eth0 down
 # ifconfig eth0 hw ether 00:11:22:33:44:55
 # ifconfig eth0 up
@@ -15,3 +15,8 @@ class Mac_changer:
 
         cmd_result = output.stdout.decode('utf-8')
         print(cmd_result)
+
+        pattern = r'ether\s[\da-z]{2}:[\da-z]{2}:[\da-z]{2}:[\da-z]{2}:[\da-z]{2}'
+        regex = re.compile(pattern)
+        ans = regex.search(cmd_result)
+        print(ans)
