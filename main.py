324471c0ef46e0c40281ca1272957b762bcb984a
Hieu Tran <hieu.tran@enouvo.com>
2023-10-24 09:16:41 +0700

    show mac addess

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from changeMacAddress import Mac_changer
 
 if __name__ == '__main__':
     mc = Mac_changer()
-    mc.get_MAC_Address("eth0")
+    changeMacAddress = mc.get_MAC_Address("eth0")
+    print(changeMacAddress)
