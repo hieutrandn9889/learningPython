d8ade6d0d962bd79720a56ede343d6557606b979
Hieu Tran <hieu.tran@enouvo.com>
2023-10-25 06:06:10 +0700

    add new address

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from changeMacAddress import Mac_changer
 if __name__ == '__main__':
     mc = Mac_changer()
     changeMacAddress = mc.get_MAC_Address("eth0")
-    current_mac = mc.change_MAC_Address("eth0", "00:11:22:33:44:55")
+    current_mac = mc.change_MAC_Address("eth0", "00:11:22:33:44:77")
