5729837e9603ceeb215b239a88a2b84485f55e46
Hieu Tran <hieu.tran@enouvo.com>
2023-10-25 06:05:33 +0700

    update

--- changeMacAddress.py
+++ changeMacAddress.py
@@ -15,10 +15,6 @@ class Mac_changer:
 
         cmd_result = output.stdout.decode('utf-8')
 
-        # ifconfig eth0
-        print(cmd_result)
-        print(1)
-
         # format eth0 address
         pattern = r'ether\s[\da-z]{2}:[\da-z]{2}:[\da-z]{2}:[\da-z]{2}:[\da-z]{2}'
         regex = re.compile(pattern)
@@ -35,11 +31,9 @@ class Mac_changer:
         print("[+] Current MAC address is", self.get_MAC_Address(iface))
         output = subprocess.run(['ifconfig', iface, "down"],
                                 shell=False, capture_output=True)
-        print(4)
         # ifconfig eth0 hw ether 00:11:22:33:44:55
         output = subprocess.run(['ifconfig', iface, "hw", "ether", new_mac],
                                 shell=False, capture_output=True)
-        print(5)
         # ifconfig eth0 up
         output = subprocess.run(['ifconfig', iface, "up"],
                                 shell=False, capture_output=True)
