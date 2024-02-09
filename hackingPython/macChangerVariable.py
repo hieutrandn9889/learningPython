05c465b35f417b40dfc0c55ac2d177248ca3de3e
Hieu Tran <hieu.tran@enouvo.com>
2024-02-10 06:16:21 +0700

    add print

--- hackingPython/macChangerVariable.py
+++ hackingPython/macChangerVariable.py
@@ -4,6 +4,8 @@ import subprocess
 interface = "eth0"
 newMac = "00:11:22:33:44:55"
 
+print("[+] Changing MAC address for " + interface + " to " + newMac)
+
 subprocess.call("ifconfig", shell=True)
 subprocess.call("ifconfig" + interface + "down", shell=True)
 subprocess.call("ifconfig" + interface + "hw ether" + newMac, shell=True)
