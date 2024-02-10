524e6e07617d5bde295aa404c4ec8cf8d450a295
Hieu Tran <hieu.tran@enouvo.com>
2024-02-10 08:58:46 +0700

    fix subprocess

--- hackingPython/handlingUserInput.py
+++ hackingPython/handlingUserInput.py
@@ -6,6 +6,6 @@ newMac = input("new MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + newMac)
 
-subprocess.call(["ifconfig " + interface + " down"])
-subprocess.call(["ifconfig " + interface + " hw ether ", newMac])
-subprocess.call(["ifconfig " + interface + " up"])
+subprocess.call(["ifconfig ", interface, " down"])
+subprocess.call(["ifconfig ", interface, " hw ether ", newMac])
+subprocess.call(["ifconfig ", interface, " up"])
