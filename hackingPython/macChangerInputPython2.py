8c799fe4030f8168f776f4f4fc462084d3f15320
Hieu Tran <hieu.tran@enouvo.com>
2024-02-10 08:33:22 +0700

    add space

--- hackingPython/macChangerInputPython2.py
+++ hackingPython/macChangerInputPython2.py
@@ -7,6 +7,6 @@ newMac = raw_input("new MAC > ")
 print("[+] Changing MAC address for " + interface + " to " + newMac)
 
 subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether" + newMac, shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
 subprocess.call("ifconfig " + interface + " up", shell=True)
 subprocess.call("ifconfig " + interface, shell=True)
