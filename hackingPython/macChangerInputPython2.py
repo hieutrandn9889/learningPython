b7e8697d5eb26f93cce5465c9ad39163dc90f38f
Hieu Tran <hieu.tran@enouvo.com>
2024-02-10 08:23:11 +0700

    input for python3 and python

---
+++ hackingPython/macChangerInputPython2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+import subprocess
+
+interface = raw_input("interface > ")
+newMac = raw_input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + newMac)
+
+subprocess.call("ifconfig ", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether" + newMac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call("ifconfig " + interface, shell=True)
---
+++ hackingPython/macChangerInputPython3.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+import subprocess
+
+interface = input("interface >")
+newMac = input("new MAC >")
+
+print("[+] Changing MAC address for " + interface + " to " + newMac)
+
+subprocess.call("ifconfig ", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether" + newMac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call("ifconfig " + interface, shell=True)
