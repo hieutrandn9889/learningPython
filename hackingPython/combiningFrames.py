f0532ee38f887f5e97e5a8e19ae86ebc67881029
Hieu Tran <hieu.tran@enouvo.com>
2024-02-13 16:37:35 +0700

    add broadcast

--- hackingPython/combiningFrames.py
+++ hackingPython/combiningFrames.py
@@ -3,8 +3,11 @@ import scapy.all as scapy
 
 
 def scan(ip):
+    # scan ip: 10.211.55.3
     arpRequest = scapy.ARP(pdst=ip)
     print(arpRequest.summary())
+    broadcastRequest = scapy.Ether()
+    scapy.ls(scapy.Ether())
 
 
 scan("10.211.55.1/24")
