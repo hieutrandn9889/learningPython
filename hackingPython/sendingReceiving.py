261fca58ee782b7b019a582a7bfc8644224490c5
Hieu Tran <hieu.tran@enouvo.com>
2024-02-14 05:25:13 +0700

    unanswered

--- hackingPython/sendingReceiving.py
+++ hackingPython/sendingReceiving.py
@@ -9,7 +9,7 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arpRequestBroadcast = broadcast/arpRequest
     answered, unanswered = scapy.srp(arpRequestBroadcast, timeout=1)
-    print(answered.summary())
+    print(unanswered.summary())
 
 
 scan("10.211.55.1/24")
