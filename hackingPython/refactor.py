b225c7a8d3dde116837e8a79e8149c1326dce7ee
Hieu Tran <hieu.tran@enouvo.com>
2024-02-13 07:00:37 +0700

    remove comment changeMac

--- hackingPython/refactor.py
+++ hackingPython/refactor.py
@@ -45,7 +45,7 @@ options = getArguments()
 currentMac = getCurrentMac(options.interface)
 print("Current Mac: " + str(currentMac))
 
-# changeMac(options.interface, options.newMac)
+changeMac(options.interface, options.newMac)
 
 if currentMac == getCurrentMac(options.newMac):
     print("[+] MAC address was successfully changed to " + currentMac)
