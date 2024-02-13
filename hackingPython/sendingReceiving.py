3da3c691dd0a7f4dd0352aa0800bb55814a11c0b
Hieu Tran <hieu.tran@enouvo.com>
2024-02-14 05:24:00 +0700

    sending and receiving

---
+++ hackingPython/sendingReceiving.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+import scapy.all as scapy
+
+
+def scan(ip):
+    # scan ip: 10.211.55.3
+    # pdst = Net ('10.211.55.1/24')
+    arpRequest = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arpRequestBroadcast = broadcast/arpRequest
+    answered, unanswered = scapy.srp(arpRequestBroadcast, timeout=1)
+    print(answered.summary())
+
+
+scan("10.211.55.1/24")
