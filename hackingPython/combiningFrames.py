1f239fa70697b9b723ca4337d280001d0b79f77c
Hieu Tran <hieu.tran@enouvo.com>
2024-02-13 16:44:05 +0700

    add show

--- hackingPython/combiningFrames.py
+++ hackingPython/combiningFrames.py
@@ -4,10 +4,13 @@ import scapy.all as scapy
 
 def scan(ip):
     # scan ip: 10.211.55.3
+    # pdst = Net ('10.211.55.1/24')
     arpRequest = scapy.ARP(pdst=ip)
-    print(arpRequest.summary())
-    broadcastRequest = scapy.Ether()
-    scapy.ls(scapy.Ether())
+    arpRequest.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arpRequestBroadcast = broadcast/arpRequest
+    arpRequestBroadcast.show()
 
 
 scan("10.211.55.1/24")
