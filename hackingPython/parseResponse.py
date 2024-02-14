f3a40c51b75a06a6c4ff667c9eefbe2bd96fe9f8
Hieu Tran <hieu.tran@enouvo.com>
2024-02-15 06:48:42 +0700

    remove comment

--- hackingPython/parseResponse.py
+++ hackingPython/parseResponse.py
@@ -8,12 +8,15 @@ def scan(ip):
     arpRequest = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arpRequestBroadcast = broadcast/arpRequest
+
+    # answeredList = scapy.srp(arpRequestBroadcast, timeout=1)[0] ==> [0] is answered
+    # answeredList = scapy.srp(arpRequestBroadcast, timeout=1)[1] ==> [1] is unanswered
     answeredList = scapy.srp(arpRequestBroadcast, timeout=1)[0]
     print("answeredList " + str(answeredList))
-    # for element in answeredList:
-    #     print(element[0].psrc)
-    #     print(element[0].hwsrc)
-    #     print("----------------------------------------------------------------")
+    for element in answeredList:
+        print(element[0].psrc)
+        print(element[0].hwsrc)
+        print("----------------------------------------------------------------")
 
 
 scan("10.211.55.1/24")
