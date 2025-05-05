aba11f8970b24462bfe0c2706aa1beb16d737401
hieutrandn9889 <hieutrandn123@gmail.com>
2025-05-05 14:15:44 +0700

    Django Admin Panel & Manipulation Of Database

--- django/myapp/learnApp/models.py
+++ django/myapp/learnApp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class Feature:
-    id: int
-    name: str
-    details: str
+class Feature(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_true = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    details = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
--- django/myapp/learnApp/views.py
+++ django/myapp/learnApp/views.py
@@ -4,23 +4,8 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our service is very quick'
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Fast'
-    feature2.details = 'Our service is very quick'
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Innovative'
-    feature3.details = 'We provide innovative solutions'
-    feature4 = Feature()
-    feature4.id = 2
-    feature4.name = 'Innovative'
-    feature4.details = 'We provide innovative solutions'
-    return render(request, 'index.html', {'feature1': feature1, 'feature2': feature2, 'feature3': feature3, 'feature4': feature4})
+        features = Feature.objects.all()  # Fetch all Feature objects from the database
+        return render(request, 'index.html', {'features': features})
 
 def static(request):
     return render(request, 'static.html', {'title': 'Welcome to MyApp'})
