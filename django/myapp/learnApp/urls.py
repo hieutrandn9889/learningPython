54c4f4276d903f855660315ad119c364ad6f688b
hieutrandn9889 <hieutrandn123@gmail.com>
2025-05-07 16:54:25 +0700

    remove file

--- django/myapp/learnApp/urls.py
+++ django/myapp/learnApp/urls.py
@@ -7,10 +7,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
-    path('course/<slug:slug>/', views.course_detail, name='course_detail'),
-    path('static/', views.static, name='static'),
-    path('variable/', views.variable, name='variable'),
-    path('counterGETInput/', views.counterGETInput, name='counterGETInput'),
-    path('GETTextarea/', views.GETTextarea, name='GETTextarea'),
-    path('POSTTextarea/', views.POSTTextarea, name='POSTTextarea'),
+    path('course/<slug:slug>/', views.course_detail, name='course_detail')
 ]
--- django/myapp/learnApp/views.py
+++ django/myapp/learnApp/views.py
@@ -131,4 +131,3 @@ def logout(request):
 def course_detail(request, slug):
     course = get_object_or_404(Course, slug=slug)
     return render(request, 'course_detail.html', {'course': course})
-
