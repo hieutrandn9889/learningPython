09f6ba116311cce0860394c1d3515048f261abcf
hieutrandn9889 <hieutrandn123@gmail.com>
2025-05-12 12:50:26 +0700

    init

---
+++ learnAIByPython/alumnium/google_test.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+from alumnium import Alumni
+from selenium.webdriver import Chrome
+from playwright.sync_api import sync_playwright
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key
+API_KEY = os.getenv("OPENAI_Test")
+if not API_KEY:
+    raise ValueError("OPENAI_Test is not set in the environment variables.")
+
+class TestGoogleSearch(unittest.TestCase):
+    def setUp(self):
+        page = sync_playwright().start().chromium.launch(headless=False).new_page()
+        page.goto("https://todomvc.com/examples/vue/dist")
+        self.al = Alumni(page)
+
+    def test_search(self):
+        self.al.do("search for 'Mercury element'")
+        self.al.do("mark all tasks complete")
+        self.al.do("delete all tasks")
